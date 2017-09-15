6b6127c5c2ad7788657e1eee3eae2e6dccf03041
Tze Ken <kenngui@hotmail.com>
2017-09-15 15:59:54 +0800

    File structure
    Established htdocs file structure and core PHP files

---
+++ htdocs/index.php
@@ -0,0 +1,38 @@
+<?php require_once (__DIR__.'/controller/init.php');?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Head -->
+        <?php 
+            $page = Param::get('page'); 
+            require_once (__DIR__.'/view/head.php'); 
+        ?>
+	</head>
+	
+	<body>
+		<!-- Navigation -->
+		<?php require_once (__DIR__.'/view/nav.php'); ?>
+		
+		<!-- Page -->
+		<?php
+            //Get page
+            if (Param::get('page')) {
+                if(file_exists(__DIR__.'/view/pages/'.Param::get('page').'.php')){
+                    require_once(__DIR__.'/view/pages/'.Param::get('page').'.php');
+                }
+                else {
+                    header('Location: error404');
+                    exit();
+                }
+            }
+            else {
+                header('Location: '.Config::get('home'));
+                exit();
+            }
+		?>
+	</body>
+	
+	<!-- Footer -->
+	<?php require_once(__DIR__.'/view/footer.php'); ?>
+</html>
--- index.php
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<?php echo "<h1>DDAC Airline Project</h1>" ?>
-</body>
-</html>
